b5cd083d1dc532fbf91eef467786cfdaeefe1103
rishabh-121 <56164057+rishabh-121@users.noreply.github.com>
2020-08-12 12:49:35 +0530

    Add files via upload

---
+++ Unicode.java
@@ -0,0 +1,15 @@
+public class Unicode {
+    public static void main(String[] args)
+    {
+        char[] ch=new char[3];
+        ch[0]='\u090B';
+        ch[1]='\u0937';
+        ch[2]='\u092D';
+        for(int i=0;i<3;i++)
+        {
+            System.out.print(ch[i]);
+        }
+
+
+    }
+}
